727a043913d7a92a5150174c4c5e3c1bb9c161e1
Weiwei Shi <shiweiwei97@gmail.com>
2016-01-19 10:53:42 +0800

    clean up server.js

--- backends/node/server.js
+++ backends/node/server.js
@@ -1,53 +1,51 @@
-#!/usr/bin/env node
+/* jslint node: true, boss: true, curly: true, eqeqeq: true, eqnull: true, expr: true,
+   immed: true, noarg: true, quotmark: single, undef: true, unused: true, sub: true
+*/
+'use strict';
 
-var ot = require('ot');
-var express = require('express');
-var morgan = require('morgan');
-var serveStatic = require('serve-static');
-var errorhandler = require('errorhandler');
-var socketIO = require('socket.io');
-var path = require('path');
-var http = require('http');
-
-var app = express();
-var appServer = http.createServer(app);
+var ot           = require('ot'),
+    morgan       = require('morgan'),
+    serveStatic  = require('serve-static'),
+    errorhandler = require('errorhandler'),
+    path         = require('path'),
+    express      = require('express'),
+    app          = express(),
+    server       = require('http').createServer(app),
+    io           = require('socket.io')(server),
+    socketIOServer;
 
 app.use(morgan('combined'));
-app.use('/', serveStatic(path.join(__dirname, '../../public')));
+app.use('/',       serveStatic(path.join(__dirname, '../../public')));
 app.use('/static', serveStatic(path.join(__dirname, '../../public')));
 if (process.env.NODE_ENV === 'development') {
-  app.use(errorhandler());
+    app.use(errorhandler());
 }
 
-var io = socketIO.listen(appServer);
-
-var str = "# This is a Markdown heading\n\n"
-        + "1. un\n"
-        + "2. deux\n"
-        + "3. trois\n\n"
-        + "Lorem *ipsum* dolor **sit** amet.\n\n"
-        + "    $ touch test.txt";
-var socketIOServer = new ot.EditorSocketIOServer(str, [], 'demo', function (socket, cb) {
-  cb(!!socket.mayEdit);
-});
-io.sockets.on('connection', function (socket) {
-  socketIOServer.addClient(socket);
-  socket.on('login', function (obj) {
-    if (typeof obj.name !== 'string') {
-      console.error('obj.name is not a string');
-      return;
+socketIOServer = new ot.EditorSocketIOServer(
+    '# This is a Markdown heading', [], 'demo',
+    function (socket, cb) {
+        cb(!!socket.mayEdit);
     }
-    socket.mayEdit = true;
-    socketIOServer.setName(socket, obj.name);
-    socket.emit('logged_in', {});
-  });
+);
+
+io.on('connection', function (socket) {
+    socketIOServer.addClient(socket);
+    socket.on('login', function (obj) {
+        if (typeof obj.name !== 'string') {
+            console.error('obj.name is not a string');
+            return;
+        }
+        socket.mayEdit = true;
+        socketIOServer.setName(socket, obj.name);
+        socket.emit('logged_in', {});
+    });
 });
 
 var port = process.env.PORT || 3000;
-appServer.listen(port, function () {
-  console.log("Listening on port " + port);
+server.listen(port, function () {
+    console.log('Listening on port ' + port);
 });
 
 process.on('uncaughtException', function (exc) {
-  console.error(exc);
+    console.error(exc);
 });
--- backends/node/web.js
+++ backends/node/web.js
@@ -1,18 +1,18 @@
 /* jslint node: true, boss: true, curly: true, eqeqeq: true, eqnull: true, expr: true,
    immed: true, noarg: true, quotmark: single, undef: true, unused: true, sub: true
 */
+'use strict';
 
 var express      = require('express'),
     morgan       = require('morgan'),
     serveStatic  = require('serve-static'),
     errorhandler = require('errorhandler'),
     path         = require('path'),
-    http         = require('http'),
     app          = express(),
-    appServer    = http.createServer(app);
+    appServer    = require('http').createServer(app);
 
 app.use(morgan('combined'));
-app.use('/', serveStatic(path.join(__dirname, '../../public')));
+app.use('/',       serveStatic(path.join(__dirname, '../../public')));
 app.use('/static', serveStatic(path.join(__dirname, '../../public')));
 if (process.env.NODE_ENV === 'development') {
     app.use(errorhandler());
